7aa20400f7b53121577310d9de3ffb26f4ac0da4
Michael Cuddy <pryankster@hotmail.com>
2022-12-30 12:03:57 -0800

    initial checkin

---
+++ src/main/java/studio/immobilehome/dabloons/Dabloons.java
@@ -0,0 +1,33 @@
+package studio.immobilehome.dabloons;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Dabloons implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("immobilehome-dabloons");
+
+	public static final Item DABLOON = new Item( new FabricItemSettings());
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+
+		LOGGER.info("Hello Fabric world from immobilehome-dabloons!");
+
+		Registry.register(Registries.ITEM, new Identifier("immobilehome-dabloons", "dabloon"), DABLOON );
+		
+	}
+}
--- src/main/java/studio/immobilehome/dabloons/mixin/DabloonsMixin.java
+++ src/main/java/studio/immobilehome/dabloons/mixin/DabloonsMixin.java
@@ -1,6 +1,6 @@
-package net.fabricmc.example.mixin;
+package studio.immobilehome.dabloons.mixin;
 
-import net.fabricmc.example.ExampleMod;
+import studio.immobilehome.dabloons.Dabloons;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -8,9 +8,9 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
-public class ExampleMixin {
+public class DabloonsMixin {
 	@Inject(at = @At("HEAD"), method = "init()V")
 	private void init(CallbackInfo info) {
-		ExampleMod.LOGGER.info("This line is printed by an example mod mixin!");
+		Dabloons.LOGGER.info("This line is printed by the Dabloons mod mixin!");
 	}
 }
